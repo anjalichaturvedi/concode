2de60fba5a83cb6b7836913530748d1c990c27e5
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2022-08-07 02:02:19 +0530

    popup + new des

--- main.js
+++ main.js
@@ -9,53 +9,40 @@ $(function() {
 });
 
 // scroll animation
-var body = document.getElementsByTagName('body')[0];
-body.style.backgroundColor = '#E1EBFF';
+// var body = document.getElementsByTagName('body')[0];
+// body.style.backgroundColor = '#E1EBFF';
 
 // trigger this function every time the user scrolls
-window.onscroll = function (event) {
-    var scroll = window.pageYOffset;
-    if (scroll < 300) {
-        // green
-        body.style.backgroundColor = '#E1EBFF';
-    } else if (scroll >= 300 && scroll < 600) {
-        // yellow
-        body.style.backgroundColor = 'rgba(211,215,176,1)';
-    } else if (scroll >= 600 && scroll < 1200) {
-        // blue
-        body.style.backgroundColor = 'blue';
-    } else if (scroll >= 1200 && scroll < 1800) {
-        // orange
-        body.style.backgroundColor = 'orange';
-    } else if (scroll >= 1800 && scroll < 3000) {
-        // red
-        body.style.backgroundColor = 'red';
-    } else {
-        // purple
-        body.style.backgroundColor = 'purple';
-    }
-}
-
-// slick
-$(document).ready(function(){
-  $('.sponsors').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1300,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2
-        }
-      }
-    ]
-  });
-});
+// window.onscroll = function (event) {
+//     var scroll = window.pageYOffset;
+//     if (scroll < 300) {
+//         // green
+//         body.style.backgroundColor = '#E1EBFF';
+//     } else if (scroll >= 300 && scroll < 600) {
+//         // yellow
+//         body.style.backgroundColor = 'rgba(211,215,176,1)';
+//     } else if (scroll >= 600 && scroll < 1200) {
+//         // blue
+//         body.style.backgroundColor = 'blue';
+//     } else if (scroll >= 1200 && scroll < 1800) {
+//         // orange
+//         body.style.backgroundColor = 'orange';
+//     } else if (scroll >= 1800 && scroll < 3000) {
+//         // red
+//         body.style.backgroundColor = 'red';
+//     } else {
+//         // purple
+//         body.style.backgroundColor = 'purple';
+//     }
+// }
 
 // owl
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
+});
+
+// cookie
+var closeButtons = $('.close');
+closeButtons.on('click', function() {
+  $(this).parent().hide();
 });
