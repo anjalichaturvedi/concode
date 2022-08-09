a13f8e950c0c01671ed6f8a43bc245182e4846ce
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2022-08-09 20:59:52 +0530

    spons

--- main.js
+++ main.js
@@ -37,9 +37,18 @@ $(function() {
 // }
 
 // owl
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
 });
+
 
 // cookie
 var closeButtons = $('.close');
