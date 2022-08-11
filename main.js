fc21a1ac9719fa9c1d0e07027ec48c08329c1e4a
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2022-08-11 22:55:44 +0530

    cookie

--- main.js
+++ main.js
@@ -36,19 +36,6 @@ $(function() {
 //     }
 // }
 
-// owl
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:4, 
-  // items change number for slider display on desktop
-  
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true
-});
-
 
 // cookie
 var closeButtons = $('.close');
